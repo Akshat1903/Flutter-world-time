3ec2f73fd5e339b6ddcbd169ff38090f6934866b
Akshat Gupta <akshatgupta1903@gmail.com>
2020-10-19 00:11:40 +0530

    code added

---
+++ flutter_weather_app/lib/pages/loading.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/services/world_time.dart';
+
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'India', flag: 'India', url: 'Asia/Kolkata');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
+    );
+  }
+}
